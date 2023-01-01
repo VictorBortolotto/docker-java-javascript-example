9c8143844fd922c9e758b1cd9f005f20941b98e2
unknown <victorcamposbortolotto@gmail.com>
2023-01-01 16:38:37 -0300

    New changes!

--- docker-web-frontend-backend-app/docker-backend-api/src/main/resources/sql/schema.sql
+++ docker-web-frontend-backend-app/docker-backend-api/src/main/resources/sql/schema.sql
@@ -1,1 +1,14 @@
 create database docker_example_app;
+
+use docker_example_app;
+
+CREATE TABLE products (
+  id bigint NOT NULL AUTO_INCREMENT,
+  color varchar(400) DEFAULT NULL,
+  price double NOT NULL,
+  description varchar(400) NOT NULL,
+  length double DEFAULT NULL,
+  name varchar(400) NOT NULL,
+  scale varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
