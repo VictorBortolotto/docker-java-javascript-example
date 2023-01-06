e4e04ca0a16a22cfbc988543515f90084bc0bdce
unknown <victorcamposbortolotto@gmail.com>
2023-01-06 00:56:07 -0300

    New Changes

--- docker-web-frontend-backend-app/docker-frontend/src/public/js/search.js
+++ docker-web-frontend-backend-app/docker-frontend/src/public/js/search.js
@@ -45,18 +45,35 @@ async function onClickDialogButtonYes(){
     let dialogProducts = document.getElementById("dialog-products");
     let dialogSearchAllProducts = document.getElementById("dialog");
     dialogProducts.showModal();
-    dialogSearchAllProducts.close()
+    dialogSearchAllProducts.close();
     let listProducts = await findAllProductsToList();
+    let pages = document.getElementById('pages');
 
     if(listProducts != null){
         let ul = document.getElementById("products-list");
         let html = '';
 
-        for(let i = 0; i < listProducts.productsList.length; i++){
-            let product = listProducts.productsList[i];
-            html += createListElementInHtml(product, i);
-        }
+        if(listProducts.productsList.length <= 20){
+            for(let i = 0; i < listProducts.productsList.length; i++){
+                let products = listProducts.productsList[i];
+                html += createListElementInHtml(products, i);
+            }
+            pages.innerHTML = '<a class="page" id="page-0">1</a>';
+            addCssToPageNumbers(1);
+        }else{
+            let list = separeteProductsPerPages(listProducts.productsList);
 
+            for(let i = 0; i < list.length; i++){
+                if(list[i] === ''){
+                    list.length = list.length - 1;
+                }
+            }
+
+            let htmlPages = generatePagesNumbers(list);
+            pages.innerHTML = htmlPages;
+            addCssToPageNumbers(list.length);
+        }
+        
         ul.innerHTML = html;
 
         for(let i = 0; i < listProducts.productsList.length; i++){
@@ -65,59 +82,6 @@ async function onClickDialogButtonYes(){
     }else {
         return;
     }
-}
-
-function createListElementInHtml(product, listPosition){
-    let html = `<li class="product-list-item" id="product-list-item-${listPosition}">
-                    <div class="list-item" id="product-id-${listPosition}">${product.id}</div>
-                    <div class="list-item" id="product-name-${listPosition}">${product.productName}</div>
-                    <div class="list-item" id="product-description-${listPosition}">${product.productDescription}</div>
-                    <div class="list-item" id="product-color-${listPosition}">${product.color}</div>
-                    <div class="list-item" id="product-length-${listPosition}">${product.productLength}</div>
-                    <div class="list-item" id="product-scale-${listPosition}">${product.productScale}</div>
-                    <div class="list-item" id="product-price-${listPosition}">${product.price}</div>
-                </li>`;
-
-    return html;
-}
-
-function addCssToListElements(listPosition) {
-    let li = document.getElementById(`product-list-item-${listPosition}`);
-    li.style.display = 'flex';
-    li.style.flexDirection = 'row';
-    li.style.alignItems = 'center';
-    li.style.listStyle = 'none';
-    li.style.width = '100%';
-    li.style.height = '5%';
-    li.style.borderBottom = 'solid black 1px'
-
-    let productId = document.getElementById(`product-id-${listPosition}`);
-    productId.style.width = '10%';
-    productId.style.borderRight = 'solid black 1px';
-
-    let productName = document.getElementById(`product-name-${listPosition}`);
-    productName.style.width = '15%';
-    productName.style.borderRight = 'solid black 1px';
-
-    let productDescription = document.getElementById(`product-description-${listPosition}`);
-    productDescription.style.width = '40%';
-    productDescription.style.borderRight = 'solid black 1px';
-
-    let productColor = document.getElementById(`product-color-${listPosition}`);
-    productColor.style.width = '15%';
-    productColor.style.borderRight = 'solid black 1px';
-
-    let productLength = document.getElementById(`product-length-${listPosition}`);
-    productLength.style.width = '10%';
-    productLength.style.borderRight = 'solid black 1px';
-
-    let productScale = document.getElementById(`product-scale-${listPosition}`);
-    productScale.style.width = '10%';
-    productScale.style.borderRight = 'solid black 1px';
-    
-    let productPrice = document.getElementById(`product-price-${listPosition}`);
-    productPrice.style.width = '10%';
-
 }
 
 async function findAllProductsToList(){
@@ -226,4 +190,137 @@ function setFieldsValues(id, name, price, description, color, length, scale, pro
     color.value = product.product.color;
     length.value = product.product.productLength;
     scale.value = product.product.productScale
+}
+
+function separeteProductsPerPages(productsList){
+    let totalPages = calculateTheTotalPages(productsList);
+    let countProductsPerPage = 0;
+    let html = '';
+    let listProductsPerPage = [];
+    let count = 0;
+
+    for(let i = 0; i < totalPages; i++){
+        for(let x = 0; x < productsList.length; x++){
+            if(countProductsPerPage <= 20){
+                let product = productsList[x];
+                html += createListElementInHtml(product, x);
+                countProductsPerPage = x;
+                if (countProductsPerPage == 20) {  
+                    countProductsPerPage = countProductsPerPage + 1;
+                    break;
+                }
+            }else {
+                count = count + 1;
+                let product = productsList[countProductsPerPage];
+                if(countProductsPerPage <= (productsList.length - 1)){
+                    html += createListElementInHtml(product, countProductsPerPage);
+                }
+                countProductsPerPage = countProductsPerPage + 1;
+
+                if(count == 20){
+                    count = 0;
+                    break;
+                }
+            }
+        }
+        listProductsPerPage[i] = html;
+        html = '';
+    }
+
+    return listProductsPerPage;
+}
+
+function calculateTheTotalPages(productsList){
+    let length = 0;
+
+    if(productsList.length % 2 == 1){
+        length = (productsList.length / 20) + 1;
+    }else{
+        length = productsList.length / 20;
+    }
+
+    return length;
+}
+
+function createListElementInHtml(product, listPosition){
+    let html = `<li class="product-list-item" id="product-list-item-${listPosition}">
+                    <div class="list-item" id="product-id-${listPosition}">${product.id}</div>
+                    <div class="list-item" id="product-name-${listPosition}">${product.productName}</div>
+                    <div class="list-item" id="product-description-${listPosition}">${product.productDescription}</div>
+                    <div class="list-item" id="product-color-${listPosition}">${product.color}</div>
+                    <div class="list-item" id="product-length-${listPosition}">${product.productLength}</div>
+                    <div class="list-item" id="product-scale-${listPosition}">${product.productScale}</div>
+                    <div class="list-item" id="product-price-${listPosition}">${product.price}</div>
+                </li>`;
+
+    return html;
+}
+
+function addCssToListElements(listPosition) {
+    let li = document.getElementById(`product-list-item-${listPosition}`);
+    li.style.display = 'flex';
+    li.style.flexDirection = 'row';
+    li.style.alignItems = 'center';
+    li.style.listStyle = 'none';
+    li.style.width = '100%';
+    li.style.height = '6%';
+    li.style.borderBottom = 'solid black 1px'
+
+    let productId = document.getElementById(`product-id-${listPosition}`);
+    productId.style.width = '10%';
+    productId.style.borderRight = 'solid black 1px';
+
+    let productName = document.getElementById(`product-name-${listPosition}`);
+    productName.style.width = '15%';
+    productName.style.borderRight = 'solid black 1px';
+
+    let productDescription = document.getElementById(`product-description-${listPosition}`);
+    productDescription.style.width = '40%';
+    productDescription.style.borderRight = 'solid black 1px';
+
+    let productColor = document.getElementById(`product-color-${listPosition}`);
+    productColor.style.width = '15%';
+    productColor.style.borderRight = 'solid black 1px';
+
+    let productLength = document.getElementById(`product-length-${listPosition}`);
+    productLength.style.width = '10%';
+    productLength.style.borderRight = 'solid black 1px';
+
+    let productScale = document.getElementById(`product-scale-${listPosition}`);
+    productScale.style.width = '10%';
+    productScale.style.borderRight = 'solid black 1px';
+    
+    let productPrice = document.getElementById(`product-price-${listPosition}`);
+    productPrice.style.width = '10%';
+
+}
+
+function generatePagesNumbers(listPages){
+    let page = 0;
+    let html = '';
+
+    for(let i = 0; i < listPages.length; i++){
+        if(listPages[i] !== ''){
+            page = page + 1
+            if(i == 0){
+                html += `<a class="page" id="page-${i}">${page}</a>`
+            }else {
+                html += `<a class="page" id="page-${i}">${page}</a>`
+            }
+        }
+    }
+
+    return html;
+}
+
+function addCssToPageNumbers(totalPages){
+    for(let i = 0; i < totalPages; i++){
+        let a = document.getElementById(`page-${i}`);
+        if(i == 0){
+            a.style.marginRight = '5px';
+            a.style.marginLeft = '5px';
+        }else{
+            a.style.marginRight = '5px';
+        }
+    }
 }
